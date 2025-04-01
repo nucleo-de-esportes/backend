871370e7f8a6dc39f8defe9cf7d7bf1da09003fa
Dante BLima <dante.borges@sempreceub.com>
2025-03-31 22:42:00 -0300

    using env variables to connect to supabase

--- config/supabase.go
+++ config/supabase.go
@@ -1,13 +1,24 @@
 package config
 
 import (
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/joho/godotenv"
 	"github.com/nedpals/supabase-go"
 )
 
 func InitSupabase() *supabase.Client {
 
-	supaUrl := "https://txuubteajinfbretuxob.supabase.co"
-	supaKey := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR4dXVidGVhamluZmJyZXR1eG9iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE3MDAxNjMsImV4cCI6MjA1NzI3NjE2M30.Nff7OKkEEcISpcDaz6zA10aAd2Etw7YrFOFuC4EYosQ"
+	err := godotenv.Load(filepath.Join("..", "supabase.env"))
+
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	supaUrl := os.Getenv("SUPABASE_URL")
+	supaKey := os.Getenv("SUPABASE_KEY")
 
 	return supabase.CreateClient(supaUrl, supaKey)
 

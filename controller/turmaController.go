cf7bedea0adaa427a86b77788657b04591e682aa
Dante BLima <dante.borges@sempreceub.com>
2025-05-05 21:35:32 -0300

    Changing response structure to return local and modalidade names instead of id

--- controller/turmaController.go
+++ controller/turmaController.go
@@ -5,6 +5,7 @@ import (
 	"strconv"
 
 	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/copier"
 	"github.com/nedpals/supabase-go"
 	"github.com/nucleo-de-esportes/backend/model"
 )
@@ -17,6 +18,29 @@ type Turma struct {
 	Sigla           string `json:"sigla"`
 	Local_Id        int64  `json:"local_id"`
 	Modalidade_Id   int64  `json:"modalidade_id"`
+}
+
+type TurmaResponse struct {
+	Horario_Inicio  string `json:"horario_inicio"`
+	Horario_Fim     string `json:"horario_fim"`
+	LimiteInscritos int64  `json:"limite_inscritos"`
+	Dia_Semana      string `json:"dia_semana"`
+	Sigla           string `json:"sigla"`
+	Local_nome      string `json:"local"`
+	Modalidade_nome string `json:"modalidade"`
+}
+
+type NomeResponse struct {
+	Nome string `json:"nome"`
+}
+
+func ConvertToTurmaResponse(turma Turma, localNome string, modalidadeNome string) TurmaResponse {
+	var response TurmaResponse
+
+	copier.Copy(&response, &turma)
+	response.Local_nome = localNome
+	response.Modalidade_nome = modalidadeNome
+	return response
 }
 
 func CreateTurma(c *gin.Context, supabase *supabase.Client) {
@@ -36,12 +60,20 @@ func CreateTurma(c *gin.Context, supabase *supabase.Client) {
 	}
 
 	var validateLocal []model.Local
+	var validadeModalidade []model.Modalidade
 
 	localIdString := strconv.FormatInt(newTurma.Local_Id, 10)
+	modalideIdString := strconv.FormatInt(newTurma.Modalidade_Id, 10)
 
 	invalidId := supabase.DB.From("local").Select("*").Eq("local_id", localIdString).Execute(&validateLocal)
+	invalidId2 := supabase.DB.From("modalidade").Select("*").Eq("modalidade_id", modalideIdString).Execute(&validadeModalidade)
 
 	if invalidId != nil || len(validateLocal) == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Local não encontrado"})
+		return
+	}
+
+	if invalidId2 != nil || len(validadeModalidade) == 0 {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "Local não encontrado"})
 		return
 	}
@@ -54,9 +86,26 @@ func CreateTurma(c *gin.Context, supabase *supabase.Client) {
 		return
 	}
 
+	var localName []NomeResponse
+	var modalidadeName []NomeResponse
+
+	errLocating := supabase.DB.From("local").Select("nome").Eq("local_id", localIdString).Execute(&localName)
+	if errLocating != nil || len(localName) == 0 {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Erro ao buscar nome do local"})
+		return
+	}
+
+	errLocating2 := supabase.DB.From("modalidade").Select("nome").Eq("modalidade_id", modalideIdString).Execute(&modalidadeName)
+	if errLocating2 != nil || len(modalidadeName) == 0 {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Erro ao buscar nome da modalidade"})
+		return
+	}
+
+	turmaResponse := ConvertToTurmaResponse(newTurma, localName[0].Nome, modalidadeName[0].Nome)
+
 	c.JSON(http.StatusCreated, gin.H{
 		"message": "Turma criada com sucesso",
-		"data":    result,
+		"data":    turmaResponse,
 	})
 
 }
@@ -125,7 +174,28 @@ func GetAllTurmas(c *gin.Context, supabase *supabase.Client) {
 		return
 	}
 
-	c.JSON(http.StatusOK, turmas)
+	var turmasResponse []TurmaResponse
+
+	for _, turma := range turmas {
+
+		localIdString := strconv.FormatInt(turma.Local_Id, 10)
+		modalidadeIdString := strconv.FormatInt(turma.Modalidade_Id, 10)
+
+		var localName []NomeResponse
+		var modalidadeName []NomeResponse
+
+		errLoc := supabase.DB.From("local").Select("nome").Eq("local_id", localIdString).Execute(&localName)
+		errMod := supabase.DB.From("modalidade").Select("nome").Eq("modalidade_id", modalidadeIdString).Execute(&modalidadeName)
+
+		if errLoc != nil || len(localName) == 0 || errMod != nil || len(modalidadeName) == 00 {
+			continue
+		}
+
+		convertResponse := ConvertToTurmaResponse(turma, localName[0].Nome, modalidadeName[0].Nome)
+		turmasResponse = append(turmasResponse, convertResponse)
+	}
+
+	c.JSON(http.StatusOK, turmasResponse)
 }
 
 func UpdateTurma(c *gin.Context, supabase *supabase.Client) {

24674240122f6a16f2b0a7302b721ea400da7817
LFguerra1 <132462586+LFguerra1@users.noreply.github.com>
2025-06-18 14:30:09 -0300

    Update main.go

--- main/main.go
+++ main/main.go
@@ -34,7 +34,14 @@ func main() {
 
 	router := gin.Default()
 
-	router.Use(cors.Default())
+	router.Use(cors.New(cors.Config{
+    		AllowOrigins:     []string{"*"}, 
+    		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+    		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+    		ExposeHeaders:    []string{"Content-Length"},
+    		AllowCredentials: true,
+    		MaxAge:           12 * time.Hour,
+	}))
 
 	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 

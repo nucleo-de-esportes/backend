7e18dd43ec3e0cd177c2151417fe84e259f687b5
Alexsandro Júnior <jalexsandro2005@gmail.com>
2025-09-26 10:15:21 -0300

    fix: adicionado atributos Port e Name a struct de config do banco de dados

--- internal/config/config.go
+++ internal/config/config.go
@@ -7,6 +7,8 @@ import (
 
 type DatabaseConfig struct {
 	Host     string
+	Port     string
+	Name     string
 	User     string
 	Password string
 }
@@ -27,7 +29,9 @@ func LoadConfig() *Config {
 		},
 		DB: DatabaseConfig{
 			Host:     getEnv("DB_HOST", "localhost"),
-			User:     getEnv("DB_USER", ""),
+			Port:     getEnv("DB_PORT", "5432"),
+			Name:     getEnv("BB_NAME", ""),
+			User:     getEnv("DB_USER", "postgres"),
 			Password: getEnv("DB_PASSWORD", ""),
 		},
 	}
